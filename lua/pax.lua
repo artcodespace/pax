ac5d29abdbd5e4d87819a72084e45b148505a376
alunturner <alun.turner@googlemail.com>
2025-02-08 22:05:47 +0000

    remove the new colour experiment

--- lua/pax.lua
+++ lua/pax.lua
@@ -1,6 +1,5 @@
 local pax_colors = {
 	hibiscus = "#ff007b",
-	ice = "#19e5e6",
 	red = "#e61919",
 	orange = "#e68019",
 	green = "#14b814",
@@ -27,7 +26,7 @@ local dark_theme = {
 	fg_minus_minus = pax_colors.grey70,
 	fg_minus = pax_colors.grey80,
 	fg = pax_colors.grey90,
-	cursor_bg = pax_colors.ice,
+	cursor_bg = pax_colors.hibiscus,
 	error = pax_colors.red,
 	warning = pax_colors.orange,
 	success = pax_colors.green,
